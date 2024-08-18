7afcef6f2f9feed32ae9cd86e7c47f6612070d09
Kevin Woodfield <kcwoodfield@gmail.com>
2024-08-18 14:25:54 -0500

    Table component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,10 +47,54 @@ interface Book {
   }[];
 }
 
-export default function Home() {
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+async function fetchBooks(): Promise<Book[]> {
+  const response = await fetch("http://localhost:4000/books");
+  if (!response.ok) {
+    throw new Error("Failed to fetch books");
+  }
+  return response.json();
+}
+
+export default async function Home() {
+  const books = await fetchBooks();
+
   return (
     <main>
-      home
+      <div className="container mx-auto grid grid-cols-1 gap-8">
+
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Title</TableHead>
+                  <TableHead>Author</TableHead>
+                  <TableHead>Pages</TableHead>
+                  <TableHead>Rating</TableHead>
+                </TableRow>
+              </TableHeader>
+
+              <TableBody>
+                {books.map(book => (
+                   <TableRow key={book.isbn}>
+                    <TableCell className="font-medium">{book.title}</TableCell>
+                    <TableCell>{book.author}</TableCell>
+                    <TableCell>{book.pages}</TableCell>
+                    <TableCell>{book.rating.average_rating}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+
+      </div>
     </main>
   );
 }
